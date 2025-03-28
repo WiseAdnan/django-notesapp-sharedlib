3888375034195d6e4c139b4027f64484c9ebcae6
WiseAdnan <adnanshahab24@gmail.com>
2025-03-29 00:54:20 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,6 +1,6 @@
 def call(String branch, String credentialsId, String url) {
- branch: "${branch}",
- credentialsId: "${credentialsId}",
+ branch: "${branch}"
+ credentialsId: "${credentialsId}"
  url: "${url}"
  echo "Code clone successfull"
 }
